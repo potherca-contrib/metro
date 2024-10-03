2774559216753401dd138b00db4f8bc9a178aa86
Auke van Slooten <auke@muze.nl>
2024-10-03 15:31:42 +0200

    removed oauth2 middleware, now has its own github repo muze-nl/metro-oauth2

--- src/everything.mjs
+++ src/everything.mjs
@@ -1,13 +1,12 @@
 import * as metro from './metro.mjs'
 import * as assert from './assert.mjs'
 import jsonmw from './mw/json.mjs'
-import oauth2 from './mw/oauth2.mjs'
 import thrower from './mw/thrower.mjs'
 
-window.metro = metro
 window.assert = assert
-window.metro.mw = {
-	jsonmw,
-	oauth2,
-	thrower
-}
+window.metro = Object.assign({}, metro, {
+	mw: {
+		jsonmw,
+		thrower
+	}
+})
