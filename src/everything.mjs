bd009a529f192d65eb9b9794f7d8955f6be9a179
Auke van Slooten <auke@muze.nl>
2024-10-11 15:04:59 +0200

    fixed old assert dep

--- src/everything.mjs
+++ src/everything.mjs
@@ -2,7 +2,6 @@ import * as metro from './metro.mjs'
 import jsonmw from './mw/json.mjs'
 import thrower from './mw/thrower.mjs'
 
-window.assert = assert
 window.metro = Object.assign({}, metro, {
 	mw: {
 		jsonmw,
